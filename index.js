f5a143ff80470af3e77548c77400c3278b36dda3
nichaaakorn <s6030213039@phuket.psu.ac.th>
2019-04-26 14:50:11 +0700

    แก้ไข

---
+++ index.js
@@ -0,0 +1,9 @@
+function verify(){
+    var userID = $('#username').val();
+    var passID = $('#password').val();
+if(userID == passID){
+    console.log('true!!!!');
+}else{
+    console.log('false!!!!');
+}
+}
