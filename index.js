7922f70cadad9c46bd6726cf82702051517a74e6
nichaaakorn <50010502+nichaaakorn@users.noreply.github.com>
2019-05-12 19:22:53 +0700

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function verify(){
-    var userID = $('#username').val();
-    var passID = $('#password').val();
-if(userID == passID){
-    console.log('true!!!!');
-}else{
-    console.log('false!!!!');
-}
-}
